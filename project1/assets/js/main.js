45cdc472f07dbda6224274b617119678eeb2a7fb
Hosem <howsem@cca.edu>
2019-02-07 13:49:12 -0800

    Updated

---
+++ project1/assets/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+
+ $(".full").toggleClass("full")
+
+
+ 	let A = $(".letter.A .makefull")
+ 	let B = $(".letter.B .makefull")
+ 	let C = $(".letter.C .makefull")
+ 	let D = $(".letter.D .makefull")
+ 	let E = $(".letter.E .makefull")
+ 	let F = $(".letter.F .makefull")
+ 	let G = $(".letter.G .makefull")
+ 	let H = $(".letter.H .makefull")
+ 	let I = $(".letter.I .makefull")
+ 	let J = $(".letter.J .makefull")
+ 	let K = $(".letter.K .makefull")
+ 	let L = $(".letter.L .makefull")
+ 	let M = $(".letter.M .makefull")
+ 	let N = $(".letter.N .makefull")
+ 	let O = $(".letter.O .makefull")
+ 	let P = $(".letter.P .makefull")
+ 	let Q = $(".letter.Q .makefull")
+ 	let R = $(".letter.R .makefull")
+  $(".letter.A").hover(function(){ 	
+  A.toggleClass("full");
+  });
+  $(".letter.B").hover(function(){ 	
+  B.toggleClass("full");
+  });
+  $(".letter.C").hover(function(){ 	
+  C.toggleClass("full");
+  });
+  $(".letter.D").hover(function(){ 	
+  D.toggleClass("full");
+  });
+  $(".letter.E").hover(function(){ 	
+  E.toggleClass("full");
+  });
+  $(".letter.F").hover(function(){ 	
+  F.toggleClass("full");
+  });
+  $(".letter.G").hover(function(){ 	
+  G.toggleClass("full");
+  });
+  $(".letter.H").hover(function(){ 	
+  H.toggleClass("full");
+  });
+  $(".letter.I").hover(function(){ 	
+  I.toggleClass("full");
+  });
+  $(".letter.J").hover(function(){ 	
+  J.toggleClass("full");
+  });
+  $(".letter.K").hover(function(){ 	
+  K.toggleClass("full");
+  });
+  $(".letter.L").hover(function(){ 	
+  L.toggleClass("full");
+  });
+  $(".letter.M").hover(function(){ 	
+  M.toggleClass("full");
+  });
+  $(".letter.N").hover(function(){ 	
+  N.toggleClass("full");
+  });
+
+  
+
+});
